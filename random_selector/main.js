39fcc21de02b6101669d34daaa598da6b667bc29
joelin123 <joeboy5258@gmail.com>
2021-05-06 17:34:49 +0800

    2

--- random_selector/main.js
+++ random_selector/main.js
@@ -1,11 +1,16 @@
 $(function(){
     $("input").on("click",function(){
-
-var numberOfListItem = $("#choices li").length;
+//alert("Hi")
+//$("h1").text("Hello")
+// $("h1").text($("li:first").text());
+// $("h1").text($("li:last").text());
+//$("h1").text($("li").eq(0).text());
+var numberOfListItem = $("li#food").length;
 var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
-$("h1").text($("#choices li").eq(randomChildNumber).text());
-
-
+$("h1").text($("li#food").eq(randomChildNumber).text());
+    //找到img元件
+    //設定img元件的src屬性-> 對應元件的title值
+$("img").attr("src",$("li#food").eq(randomChildNumber).attr("title"));
 
 });
 
